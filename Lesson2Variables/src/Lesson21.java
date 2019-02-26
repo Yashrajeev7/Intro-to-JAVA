d948531b787431ce6a4f10296cccac1faeed0df8
yrajeev <yrajeev@HS-D210-03.MOHS.local>
2019-02-26 12:13:17 -0500

    hello world

--- Lesson2Variables/src/Lesson21.java
+++ Lesson2Variables/src/Lesson21.java
@@ -3,15 +3,26 @@ public class Lesson21 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     int ageI =17;
-     double heightI = 5.6;
-     String weightI = "138 lbs";
-     String GPAI = "5.33";
-     System.out.println("Age = "+ageI);
-     System.out.println("Height = "+heightI);
-     System.out.println("Weight ="+weightI);
-     System.out.println("GPA =" +GPAI);
      
+     
+     
+ System.out.println(" All \"good\" men should come to the aid of their country");
+ 
+ System.out.println("Hello \nHello again");
+     
+ System.out.println("a backslash looks like this \\,... rihgt?");
+    
+ String pq = "Eddie Haskel";
+ int hm = pq.length();
+ String ed = pq.substring(hm -4);
+ System.out.println(ed);
+ 
+ String s1 = "Allan Alda";
+ String s2 = "John Wayne";
+ String s3 = "Gregory Peck";
+ 
+ System.out.println(s1.substring(2));
+ 
      
      
 	}
